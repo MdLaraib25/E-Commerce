2218d4ef0f428177dd8b860c37f266ef970a4614
MdLaraib25 <mdlaraib85@gmail.com>
2026-04-08 16:00:19 +0530

    done with select size

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
@@ -8,31 +10,33 @@ import Product from './pages/Product'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
-import Navbar from './components/Navbar'
 import Cart from './pages/Cart'
+
+import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] '>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
       <Navbar />
       <SearchBar />
+
       <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/collection' element = {<Collection />} />
-        <Route path='/about' element = {<About />} />
-        <Route path='/contact' element = {<Contact />} />
-        <Route path='/product/:productId' element = {<Product />} />
-        <Route path='/cart' element = {<Cart />} />
-        <Route path='/login' element = {<Login />} />
-        <Route path='/place-order' element = {<PlaceOrder />} />
-        <Route path='/orders' element = {<Orders />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
       </Routes>
+
       <Footer />
     </div>
-    
-    
   )
 }
 
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -10,7 +11,12 @@ const ShopContextProvider = (props) => {
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     
-    const  addToCard = async (itemId, size) => {
+    const  addToCart = async (itemId, size) => {
+        if (!size) {
+            toast.error('Please select a size');
+            return;
+        }
+
         let cartData= structuredClone(cartItems);
         if (cartData[itemId]) {
             if (cartData[itemId][size]) {
@@ -23,21 +29,35 @@ const ShopContextProvider = (props) => {
         }
         setCartItems(cartData);
     }
+    
+    const getCartCount = () => {
+        let count = 0;
+        for (const itemId in cartItems) {
+            for (const size in cartItems[itemId]) {
+                try {
+                    if (cartItems[itemId][size] > 0) {
+                        count += cartItems[itemId][size];
+                    } 
+                } catch (error) {}
+            }
+        }
+        return count;
+    }
 
     const value = {
         products, currency , delivery_fee,
         search,setSearch, showSearch, setShowSearch, cartItems
-        , addToCard
+        , addToCart, getCartCount
     }
 
-    useEffect(() => {
-        console.log(cartItems);
+    // useEffect(() => {
+    //     console.log(cartItems);
 
-        // const cartData = localStorage.getItem('cart');
-        // if (cartData) {
-        //     setCartItems(JSON.parse(cartData));
-        // }
-    }, [cartItems]);
+    //     // const cartData = localStorage.getItem('cart');
+    //     // if (cartData) {
+    //     //     setCartItems(JSON.parse(cartData));
+    //     // }
+    // }, [cartItems]);
 
     return (
         <ShopContext.Provider value={value}>
