8b73718c0eba0d4698dcdb046035e312aab3c924
MdLaraib25 <mdlaraib85@gmail.com>
2026-04-08 19:24:50 +0530

    done with cart function

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,60 +9,92 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
-    const [cartItems, setCartItems] = useState([]);
-    
-    const  addToCart = async (itemId, size) => {
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId, size) => {
         if (!size) {
             toast.error('Please select a size');
             return;
         }
 
-        let cartData= structuredClone(cartItems);
+        const cartData = structuredClone(cartItems);
         if (cartData[itemId]) {
-            if (cartData[itemId][size]) {
-                cartData[itemId][size] += 1;
-            } else {
-                cartData[itemId][size] = 1;
-            }
+            cartData[itemId][size] = (cartData[itemId][size] || 0) + 1;
         } else {
             cartData[itemId] = { [size]: 1 };
         }
+
         setCartItems(cartData);
     }
-    
+
+    const updateQuantity = (itemId, size, quantity) => {
+        const cartData = structuredClone(cartItems);
+        if (!cartData[itemId]) cartData[itemId] = {};
+
+        if (quantity <= 0) {
+            delete cartData[itemId][size];
+            if (Object.keys(cartData[itemId]).length === 0) delete cartData[itemId];
+        } else {
+            cartData[itemId][size] = quantity;
+        }
+
+        setCartItems(cartData);
+    }
+
     const getCartCount = () => {
         let count = 0;
         for (const itemId in cartItems) {
             for (const size in cartItems[itemId]) {
-                try {
-                    if (cartItems[itemId][size] > 0) {
-                        count += cartItems[itemId][size];
-                    } 
-                } catch (error) {}
+                const quantity = cartItems[itemId][size];
+                if (quantity > 0) count += quantity;
             }
         }
         return count;
     }
 
-    const value = {
-        products, currency , delivery_fee,
-        search,setSearch, showSearch, setShowSearch, cartItems
-        , addToCart, getCartCount
+    const getCartAmount = () => {
+        let totalAmount = 0;
+        for (const itemId in cartItems) {
+            const itemInfo = products.find(product => product._id === itemId);
+            if (!itemInfo) continue;
+            for (const size in cartItems[itemId]) {
+                const quantity = cartItems[itemId][size];
+                if (quantity > 0) totalAmount += quantity * itemInfo.price;
+            }
+        }
+        return totalAmount;
     }
 
-    // useEffect(() => {
-    //     console.log(cartItems);
+    // Optional: persist cart in localStorage
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cartItems));
+    }, [cartItems]);
 
-    //     // const cartData = localStorage.getItem('cart');
-    //     // if (cartData) {
-    //     //     setCartItems(JSON.parse(cartData));
-    //     // }
-    // }, [cartItems]);
+    useEffect(() => {
+        const cartData = localStorage.getItem('cart');
+        if (cartData) setCartItems(JSON.parse(cartData));
+    }, []);
+
+    const value = {
+        products,
+        currency,
+        delivery_fee,
+        search,
+        setSearch,
+        showSearch,
+        setShowSearch,
+        cartItems,
+        addToCart,
+        getCartCount,
+        updateQuantity,
+        getCartAmount
+    }
 
     return (
         <ShopContext.Provider value={value}>
             {props.children}
         </ShopContext.Provider>
-    )
+    );
 }
+
 export default ShopContextProvider;
